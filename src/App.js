6c7e5866a4a638faf0010f2ed69bb5e40e916129
Vishal Ekka <vishalekka18@gmail.com>
2024-02-20 11:20:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,169 @@
-import React, {useEffect, useState} from "react";
-import Particles, {initParticlesEngine} from "@tsparticles/react";
-import {loadFull} from "tsparticles";
-import logo from "./logo.svg";
-import "./App.css";
-import particlesOptions from "./particles.json";
+// import React, {useEffect, useState} from "react";
+// import Particles, {initParticlesEngine} from "@tsparticles/react";
+// import {loadFull} from "tsparticles";
+// import logo from "./logo.svg";
+// import "./App.css";
+// import particlesOptions from "./particles.json";
 
-function App() {
-    const [init, setInit] = useState(false);
+// function App() {
+//     const [init, setInit] = useState(false);
 
-    useEffect(() => {
-        if (init) {
-            return;
-        }
+//     useEffect(() => {
+//         if (init) {
+//             return;
+//         }
 
-        initParticlesEngine(async (engine) => {
-            await loadFull(engine);
-        }).then(() => {
-            setInit(true);
-        });
-    }, []);
+//         initParticlesEngine(async (engine) => {
+//             await loadFull(engine);
+//         }).then(() => {
+//             setInit(true);
+//         });
+//     }, []);
 
+//     return (
+//         <div className="App">
+//             {init && <Particles options={particlesOptions}/>}
+//             <header className="App-header">
+//                 <img src={logo} className="App-logo" alt="logo"/>
+//                 <p>
+//                     Edit <code>src/App.js</code> and save to reload.
+//                 </p>
+//                 <p>
+//                     Edit <code>src/particles.json</code> to customize Particles, then save
+//                     to reload.
+//                 </p>
+//                 <a
+//                     className="App-link"
+//                     href="https://reactjs.org"
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                 >
+//                     Learn React
+//                 </a>
+//                 <a
+//                     className="App-link"
+//                     href="https://particles.js.org"
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                 >
+//                     See Particles samples
+//                 </a>
+//             </header>
+//         </div>
+//     );
+// }
+
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+
+const App = () => {
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#0d47a1",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 6,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
+  if (init) {
     return (
-        <div className="App">
-            {init && <Particles options={particlesOptions}/>}
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    Edit <code>src/particles.json</code> to customize Particles, then save
-                    to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <a
-                    className="App-link"
-                    href="https://particles.js.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
-                </a>
-            </header>
-        </div>
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
     );
-}
+  }
+
+  return <></>;
+};
 
 export default App;
